c3a6e9a8ebc09893f3c3d3ee699654466fbf2c25
oohsai <sadyskiran@gmail.com>
2025-02-03 19:00:46 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.post("/add-liqudity", (req, res) => {
+})
+
+app.post("/buy-asset", (req, res) => {
+})
+
+app.post("/sell-asset", (req, res) => {
+})
+
+app.post("/quote", (req, res) => {
+})
